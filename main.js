4ec2ca3f8337b11ff52db3e9b9705c80c0480621
João Carlos Peliçon Ramos <peliconjoao@gmail.com>
2021-07-03 13:27:54 -0400

    versão final

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Atualizando o temporizador
 const timer = {
     pomodoro: 25,
     shortBreak: 5,
@@ -6,20 +7,22 @@ const timer = {
     sessions: 0,
   };
   
-  let interval;
-  
+  let interval; 
+
+  //Som do botão
   const buttonSound = new Audio('button-sound.mp3');
+  //Botão Começar
   const mainButton = document.getElementById('js-btn');
   mainButton.addEventListener('click', () => {
     buttonSound.play();
     const { action } = mainButton.dataset;
-    if (action === 'start') {
+    if (action === 'começar') {
       startTimer();
     } else {
       stopTimer();
     }
   });
-  
+  //Atualizando contagem regressiva de forma apropriada
   const modeButtons = document.querySelector('#js-mode-buttons');
   modeButtons.addEventListener('click', handleMode);
   
@@ -37,15 +40,15 @@ const timer = {
       seconds,
     };
   }
-  
+  //iniciando temporizador
   function startTimer() {
     let { total } = timer.remainingTime;
     const endTime = Date.parse(new Date()) + total * 1000;
   
-    if (timer.mode === 'pomodoro') timer.sessions++;
+    if (timer.mode === 'pomodoro') timer.sessions++; // Incrementando sessões de pomodoro a cada 4 ocorre uma longa pausa
   
-    mainButton.dataset.action = 'stop';
-    mainButton.textContent = 'stop';
+    mainButton.dataset.action = 'parar';
+    mainButton.textContent = 'parar';
     mainButton.classList.add('active');
   
     interval = setInterval(function() {
@@ -59,18 +62,18 @@ const timer = {
         switch (timer.mode) {
           case 'pomodoro':
             if (timer.sessions % timer.longBreakInterval === 0) {
-              switchMode('longBreak');
+              switchMode('pausaLonga');
             } else {
-              switchMode('shortBreak');
+              switchMode('pausaCurta');
             }
             break;
           default:
             switchMode('pomodoro');
         }
   
-        if (Notification.permission === 'granted') {
+        if (Notification.permission === 'permitido') {
           const text =
-            timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+            timer.mode === 'pomodoro' ? 'Volte ao trabalho!' : 'Faça uma pausa!';
           new Notification(text);
         }
   
@@ -80,15 +83,15 @@ const timer = {
       }
     }, 1000);
   }
-  
+  //Função para parar o temporizador
   function stopTimer() {
     clearInterval(interval);
   
-    mainButton.dataset.action = 'start';
-    mainButton.textContent = 'start';
+    mainButton.dataset.action = 'começar';
+    mainButton.textContent = 'começar';
     mainButton.classList.remove('active');
   }
-  
+  //Função que faz parte da contagem regressiva 
   function updateClock() {
     const { remainingTime } = timer;
     const minutes = `${remainingTime.minutes}`.padStart(2, '0');
@@ -98,15 +101,14 @@ const timer = {
     const sec = document.getElementById('js-seconds');
     min.textContent = minutes;
     sec.textContent = seconds;
-  
-    const text =
-      timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+  //mostrando contagem no titulo da pagina
+    const text = timer.mode === 'pomodoro' ? 'Volte ao trabalho!' : 'Faça uma pausa!';
     document.title = `${minutes}:${seconds} — ${text}`;
-  
+    //atualizando barra de progresso
     const progress = document.getElementById('js-progress');
     progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
   }
-  
+  //função para adicionar duas propriedades ao objeto (modo atual / modo temporizador)
   function switchMode(mode) {
     timer.mode = mode;
     timer.remainingTime = {
@@ -126,7 +128,7 @@ const timer = {
   
     updateClock();
   }
-  
+  //Função para parar temporizador quando trocar de modo
   function handleMode(event) {
     const { mode } = event.target.dataset;
   
@@ -135,15 +137,15 @@ const timer = {
     switchMode(mode);
     stopTimer();
   }
-  
+  //Exibindo notificações
   document.addEventListener('DOMContentLoaded', () => {
     if ('Notification' in window) {
       if (
-        Notification.permission !== 'granted' &&
-        Notification.permission !== 'denied'
+        Notification.permission !== 'permitido' &&
+        Notification.permission !== 'negado'
       ) {
         Notification.requestPermission().then(function(permission) {
-          if (permission === 'granted') {
+          if (permission === 'permitido') {
             new Notification(
               'incrível! Você será notificado no início de cada sessão'
             );
